722f4aa8956b7c9b02bec4091382ec02a6ef1002
antonioHigueron <acobosscabello@gmail.com>
2023-05-12 18:09:05 +0200

    add deletebuilder

--- src/main/java/io/acaeronte/jdbc/builders/WhereBuilderBase.java
+++ src/main/java/io/acaeronte/jdbc/builders/WhereBuilderBase.java
@@ -1,4 +1,30 @@
 package io.acaeronte.jdbc.builders;
 
+import io.acaeronte.jdbc.common.Condition;
+import io.acaeronte.jdbc.mapper.annotations.Column;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public abstract class WhereBuilderBase extends BuilderBase {
+
+    private final List<Condition> conditions;
+    private final Map<Column, Field> columnsFields;
+
+    public WhereBuilderBase(Map<Column, Field> columnsFields){
+        this.conditions = new ArrayList<>();
+        this.columnsFields = columnsFields;
+    }
+
+    public void addCondition(Condition condition){
+        this.conditions.add(condition);
+    }
+
+    public List<Condition> getConditions() {
+        return this.conditions;
+    }
+
+
 }
--- src/main/java/io/acaeronte/jdbc/builders/delete/DeleteBuilder.java
+++ src/main/java/io/acaeronte/jdbc/builders/delete/DeleteBuilder.java
@@ -1,6 +1,28 @@
 package io.acaeronte.jdbc.builders.delete;
 
 import io.acaeronte.jdbc.builders.WhereBuilderBase;
+import io.acaeronte.jdbc.mapper.annotations.Column;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 public class DeleteBuilder extends WhereBuilderBase {
+
+    public DeleteBuilder(Map<Column, Field> columnsFields){
+        super(columnsFields);
+    }
+
+    public String getSqlStatement(){
+        StringBuilder sql = new StringBuilder();
+        generateDelete(sql);
+        generateWhere(sql);
+        generateIsolation(sql);
+        return sql.toString();
+    }
+
+    private void generateDelete(StringBuilder sql) {
+        sql.append("DELETE FROM ");
+        sql.append(this.table);
+    }
+
 }
